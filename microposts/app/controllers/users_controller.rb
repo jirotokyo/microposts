d0570e66866bd566d70c40b24e918198049e5497
Jiro Ikeda <c2cjapan@gmail.com>
2016-08-23 15:47:12 +0000

    add posting microposts

--- microposts/app/controllers/users_controller.rb
+++ microposts/app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   
   def show # 追加
    @user = User.find(params[:id])
+   @microposts = @user.microposts.order(created_at: :desc)
   end
   
   def new
@@ -19,11 +20,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+     @user= User.find(params[:id])
+  end
+  
+  def update
+    @user= User.find(params[:id])
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to @user , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :location, :profile)
   end
   
 end
